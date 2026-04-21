5f40c4ec44c1a8050adb4fd90ae53f732cf0fa13
Mishakhurana5 <Rosykhurana01@gmail.com>
2026-04-21 20:52:21 +0530

    Add files via upload

---
+++ Printcommand.py
@@ -0,0 +1,23 @@
+print("Welcome to the world of programming!")
+print(4)
+print("Hello Python! \n")
+print("Hello!",5)
+print("welcome to", end = "*")
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "Codingal"
+print(codingal)
+
+name = input("enter your name :")
+
+print ("\nHello!", name ,"\nWelcome to codingal!")
+
+
+import keyword
+print("Python keywords are...\n")
+print(keyword.kwlist)
