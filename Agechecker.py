ad1f7edbd5ec705f7ca0d882b4476c03f8d4baed
Mishakhurana5 <Rosykhurana01@gmail.com>
2026-05-21 21:33:14 +0530

    Add files via upload

---
+++ Agechecker.py
@@ -0,0 +1,7 @@
+age = int(input("Please enter your age: "))
+if (age < 10):
+    print("You are not allowed")
+elif (age > 20):
+    print("You are not allowed")
+else: 
+    print("You are allowed")
