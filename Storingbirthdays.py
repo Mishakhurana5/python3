6677aef03eb7f3ca9d3e89a1c27ea14070dd7d15
Mishakhurana5 <Rosykhurana01@gmail.com>
2026-05-23 10:55:31 +0530

    Add files via upload

---
+++ Storingbirthdays.py
@@ -0,0 +1,11 @@
+Ram = "05/12/2014"
+Shyam = "10/25/2015"
+Amit = "08/17/2016"
+Priya = "03/09/2017"
+Sneha = "11/30/2018"
+
+print("Ram: " + Ram)
+print("Shyam: " + Shyam)
+print("Amit: " + Amit)
+print("Priya: " + Priya)
+print("Sneha: " + Sneha)
